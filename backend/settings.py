71e9e5be8e5b78cb8532bde538f3d660e68c6715
rhythm98 <rhyagr@gmail.com>
2021-01-17 23:39:25 +0530

    postgreSQL added.

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'spicyo.apps.SpicyoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kitchen-backend',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost'
     }
 }
 
@@ -123,3 +127,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- spicyo/views.py
+++ spicyo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Dish
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    
+    dishes=Dish.objects.all()
+
+    return render(request,'index.html',{'dishes':dishes})
